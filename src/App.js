4059ba68164e1801028ab3c19f365e5af33fcf32
poojapdarade <poojap.darade@gmail.com>
2024-08-07 23:59:18 +0100

    Started with 25 react projects to improve React skills

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Accordion from "./components/accordian";
 
 function App() {
+  <Accordion />;
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div>
+        <h1>StopWatch</h1>
+        <span>{"0" + Math.floor((time / 6000) % 60)}:</span>
+        <span>{"0" + Math.floor((time / 1000) % 60)}:</span>
+        <span>{"0" + ((time / 10) % 100)}:</span>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            setRunning(true);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Start
+        </button>
+        <button
+          onClick={() => {
+            setRunning(false);
+          }}
+        >
+          Stop
+        </button>
+        <button onClick={(setTime = 0)}>Reset</button>
+      </div>
+    </>
   );
 }
 
