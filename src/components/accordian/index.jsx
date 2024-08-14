f440c5f0d47f21e825e7fb34c0313b0325d27989
poojapdarade <poojap.darade@gmail.com>
2024-08-14 22:33:30 +0100

    displaying questions by fetching them from the data file by using map.

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -1,3 +1,24 @@
+import { useState } from "react";
+import data from "./data";
+
 export default function Accordion() {
-  return <div>Accordion</div>;
+  const [selected, setSelected] = useState(null);
+  return (
+    <div className="wrapper">
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div className="title">
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>Data not found</div>
+        )}
+      </div>
+    </div>
+  );
 }
