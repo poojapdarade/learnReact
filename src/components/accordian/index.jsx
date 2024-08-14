f1e89173698032f9e7f424d140dc8258ea080827
poojapdarade <poojap.darade@gmail.com>
2024-08-14 23:25:33 +0100

    Copy pasted the css file for accordian project

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -1,19 +1,32 @@
 import { useState } from "react";
 import data from "./data";
+import "./style.css";
 
 export default function Accordion() {
   const [selected, setSelected] = useState(null);
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+  console.log(selected);
+
   return (
     <div className="wrapper">
       <h2>Accordian</h2>
       <div className="accordion">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
-            <div className="item">
-              <div className="title">
+            <div className="item" key={dataItem.id}>
+              <div
+                onClick={() => handleSingleSelection(dataItem.id)}
+                className="title"
+              >
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
+
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
             </div>
           ))
         ) : (
