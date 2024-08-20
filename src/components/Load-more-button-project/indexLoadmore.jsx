6e59ac3f1883f00b9bc6d67e3bf39b5be7efe018
poojapdarade <poojap.darade@gmail.com>
2024-08-20 21:16:32 +0100

    05-Load more data project in process

--- src/components/Load-more-button-project/indexLoadmore.jsx
+++ src/components/Load-more-button-project/indexLoadmore.jsx
@@ -1,25 +1,60 @@
 import { useEffect, useState } from "react";
 
+async function fetchProducts(count) {
+  console.log(count);
+  try {
+    const response = await fetch(
+      `https://dummyjson.com/products?limit=20&skip=${
+        count === 0 ? 0 : count * 20
+      }`
+    );
+
+    const result = await response.json();
+    if (result && result.product && result.products.length) {
+      setProducts(result.products);
+      setLoading(false);
+    }
+    console.log(result);
+    return result;
+  } catch (e) {
+    console.log(e);
+    setLoading(false);
+    throw e;
+  }
+}
+
 export function LoadMoreData() {
   const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
   const [count, setCount] = useState(0);
 
-  async function fetchProducts() {
-    try {
-      const response = await fetch(
-        `https://dummyjson.com/products?limit=20&skip=${
-          count === 0 ? 0 : count * 20
-        }`
-      );
+  useEffect(() => {
+    fetchProducts(count)
+      .then((products) => {})
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [count]);
 
-      const result = await response.json;
-    } catch (e) {
-      console.log(e);
-    }
+  if (loading) {
+    return <div>Loading Data ! Please wait.</div>;
   }
 
-  useEffect(() => fetchProducts(), []);
-
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <div className="product">
+        {products && products.length
+          ? products.map((item) => (
+              <div key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
+                <p>{item.title}</p>
+              </div>
+            ))
+          : null}
+      </div>
+      <div>
+        <button className="Button Container">Load More Button</button>
+      </div>
+    </div>
+  );
 }
