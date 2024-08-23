9795d018edd9268556e38775f9de0226564610af
poojapdarade <poojap.darade@gmail.com>
2024-08-23 23:36:47 +0100

    Solved previous error- the file was meant to be js instead of jsx. Trying to give a key in menu-item file.But it is not working

--- src/components/recursiveNavigationMenu/menu-list.jsx
+++ src/components/recursiveNavigationMenu/menu-list.jsx
@@ -1,11 +1,12 @@
+import { menus } from "./data";
 import { MenuItem } from "./menu-item";
 
 export default function MenuList({ list = [] }) {
   return (
     <div className="menu-list-container">
       {list && list.length
-        ? list.map((listItem) => <MenuItem item={listItem} />)
+        ? list.map((listItem) => <MenuItem item={listItem} key={} />)
         : null}
     </div>
-  );  
+  );
 }
