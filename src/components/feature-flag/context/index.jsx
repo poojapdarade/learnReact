8cd511030b21d1d1aa9048c6d2d8d3a36e5572a7
poojapdarade <poojap.darade@gmail.com>
2024-09-04 00:02:49 +0100

    Rendered all the components using featureFalgDataSerciceCall

--- src/components/feature-flag/context/index.jsx
+++ src/components/feature-flag/context/index.jsx
@@ -1,8 +1,26 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { featureFlagsDataServiceCall } from "../data";
 
 export const FeatureFlagContext = createContext(null);
 
 export function FeatureFlagGlobalState({ children }) {
+  const [loading, setLoading] = useState(false);
+  const [enabledFlags, setEnabledFlags] = useState({});
+
+  async function fetchFeatureFlags() {
+    try {
+      const response = await featureFlagsDataServiceCall();
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+      throw new Error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchFeatureFlags();
+  }, []);
+
   return (
     <FeatureFlagContext.Provider value={{}}>
       {children}
